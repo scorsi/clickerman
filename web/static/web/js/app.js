fc7c6f6140ca70bfb608c8beb67cd50f40e652b6
yan <yan.poinssot@epitech.eu>
2016-11-14 22:16:52 +0100

    [evolution] added accueil entreprise

--- web/static/web/js/app.js
+++ web/static/web/js/app.js
@@ -24,3 +24,21 @@ function 	select_generator(id, start, end)
 function 	select_update_color(object) {
 	$(object).css('color','#f60');
 };
+
+function 	scrollTo(id)
+{
+	if ($(id).length != 0)
+	{
+		$('html, body').stop().animate({
+			scrollTop: $(id).offset().top + 1
+		}, 500, 'swing');
+		return false;
+	}
+}
+
+$(document).ready()
+{
+	$('a').click(function(){
+		scrollTo($(this).attr('href'));
+	});
+}
