3dfff6d15b9eb98059a216b6ffa5ac6ee2546cfe
Adfinitas Temp <adfinitastemp@dhcp141.adfinitas.fr>
2016-11-16 11:54:43 +0100

    [Evolution] création de compte utilisateur

--- web/forms.py
+++ web/forms.py
@@ -9,7 +9,7 @@ from .models import *
 
 class LoginForm(AuthenticationForm):
 	username = forms.CharField(label="Username", max_length=30,
-		widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'username', 'placeholder': 'Addresse email ou Username'}))
+		widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'username', 'placeholder': 'Addresse email ou Username', 'type': 'email'}))
 	password = forms.CharField(label="Password", max_length=30,
 		widget=forms.PasswordInput(attrs={'class': 'form-control', 'name': 'password', 'placeholder': 'Mot de passe'}))
 	def clean_username(self):
