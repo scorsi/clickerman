2b32a9eedde2b849249d2c599a609e982fed4d97
scorsi <sylvain.corsini@gmail.com>
2016-11-17 14:21:33 +0100

    [Evolution] Adding register view.

--- web/views.py
+++ web/views.py
@@ -1,9 +1,22 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+from .forms import RegisterForm
+from .models import User
 
 @login_required(login_url="login/")
 def home(request):
     return render(request, "home.html")
+
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse('OK!')
+    else:
+        form = RegisterForm()
+    return render(request, 'register.html', {'register_form': form})
