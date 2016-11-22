849e219fe9e8ccf4f0104fe7899d9eb6f822838b
scorsi <sylvain.corsini@gmail.com>
2016-11-22 13:22:57 +0100

    [Evolution] Adding User and Profile forms.

--- web/forms.py
+++ web/forms.py
@@ -29,3 +29,15 @@ class RegisterForm(UserCreationForm):
             'password': forms.PasswordInput(attrs={'class': 'login_input', 'name': 'password'})
         }
         """
+
+
+class UserEditForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email')
+
+
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('gender', 'medias', 'social_networks', 'bio', 'date_birthday')
