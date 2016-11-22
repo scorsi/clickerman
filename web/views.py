c5829541804e0a8e691b4d5161cd16ece6584fac
scorsi <sylvain.corsini@gmail.com>
2016-11-22 13:23:56 +0100

    [Evolution] Adding account_edit view.

--- web/views.py
+++ web/views.py
@@ -1,8 +1,9 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template.response import TemplateResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login
-from .forms import RegisterForm, LoginForm
+from .forms import RegisterForm, LoginForm, UserEditForm, ProfileEditForm
+from .models import Profile
 
 
 @login_required(login_url='login/')
@@ -33,3 +34,18 @@ def auth(request):
             'login_form': LoginForm(),
         }
     return TemplateResponse(request, 'register.html', context)
+
+
+@login_required(login_url='login/')
+def account_edit(request):
+    profile_instance = Profile.objects.get_or_create(user=request.user.id)
+    context = {
+        'user_form': UserEditForm(request.POST or None, request.FILES or None, instance=request.user),
+        'profile_form': ProfileEditForm(request.POST or None, request.FILES or None, instance=profile_instance[0]),
+    }
+    if request.method == 'POST':
+        if context['user_form'].is_valid() and context['profile_form'].is_valid():
+            context['user_form'].save()
+            context['profile_form'].save()
+            return HttpResponseRedirect('/account/edit')
+    return TemplateResponse(request, 'account_edit.html', context)
