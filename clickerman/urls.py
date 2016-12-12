22f2c0ae8a8abe7327fb319e5eaf43a6f3cb07e9
scorsi <sylvain.corsini@gmail.com>
2016-12-12 14:23:59 +0100

    [Evolution] Adding API urls.

--- clickerman/urls.py
+++ clickerman/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^', include('web.urls', namespace="web")),
+    url(r'^api/', include('api.urls', namespace="api")),
     url(r'^account/', include('account.urls', namespace="account")),
     url(r'^admin/', admin.site.urls),
 ]
